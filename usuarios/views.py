d8b712b78b02d76b1462aa54dcc54be2f882d6ab
ism4jly <ismaelpaulofreitas@gmail.com>
2024-02-01 19:56:00 -0300

    logica de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from usuarios.forms import LoginForms, CadastroForms
 
+from django.contrib.auth.models import User
+
 def login(request):
     form = LoginForms()
     return render(request, "usuarios/login.html", {"form": form})
@@ -14,7 +16,25 @@ def cadastro(request):
 
         if form["senha_1"].value() != form["senha_2"].value():
             return redirect('cadastro')
-        
+
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect('cadastro')
+
+        nome= form["nome_cadastro"].value()
+        email= form["email"].value()
+        senha= form["senha_1"].value()
+
+        if User.objects.filter(username=nome).exists():
+            return redirect('cadastro')
+
+        usuario = User.objects.create_user(
+            username=nome,
+            email=email,
+            password=senha
+        )
+        usuario.save()
+        return redirect('login')
 
     return render(request, "usuarios/cadastro.html", {"form": form})
 

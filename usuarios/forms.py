86a34379e2ff4050dcab74440348cb4005bc1149
ism4jly <ismaelpaulofreitas@gmail.com>
2024-02-01 22:27:17 -0300

    mensagem de erro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -72,11 +72,11 @@ class CadastroForms(forms.Form):
     )
 
     def clean_nome_cadastro(self):
-        nome = self.cleaned_data.get('nome_cadastro')
+        nome = self.cleaned_data.get("nome_cadastro")
 
         if nome:
             nome = nome.strip()
-            if ' ' in nome:
-                raise forms.ValidationError('Espaços não são permitidos nesse campo')
+            if " " in nome:
+                raise forms.ValidationError("Espaços não são permitidos nesse campo")
             else:
                 return nome
